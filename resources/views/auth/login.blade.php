cbe6de72d8d042f379c605fb278a0375bfd7889d
unknown <you@example.com>
2026-01-19 01:03:35 +0600

    admin login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,103 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!doctype html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <meta charset="utf-8">
+        <title>Login | Veltrix - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+        <meta content="Themesbrand" name="author">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css">
+
+    </head>
+
+<body>
+
+    <div class="home-btn d-none d-sm-block">
+        <a href="index.html" class="text-dark"><i class="fas fa-home h2"></i></a>
+    </div>
+    <div class="account-pages my-5 pt-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    <div class="card overflow-hidden">
+                        <div class="bg-primary">
+                            <div class="text-primary text-center p-4">
+                                <h5 class="text-white font-size-20">Welcome Back !</h5>
+                                <p class="text-white-50">Sign in to continue to Veltrix.</p>
+                                <a href="index.html" class="logo logo-admin">
+                                    <img src="assets/images/logo-sm.png" height="24" alt="logo">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="card-body p-4">
+                            <div class="p-3">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label class="form-label" for="username">Email</label>
+                                        <input type="text" name="email" class="form-control" id="username" placeholder="Enter username">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label class="form-label" for="userpassword">Password</label>
+                                        <input type="password" class="form-control" name="password" id="userpassword" placeholder="Enter password">
+                                    </div>
+
+                                    <div class="mb-3 row">
+                                        <div class="col-sm-6">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="customControlInline">
+                                                <label class="form-check-label" for="customControlInline">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 text-end">
+                                            <button class="btn btn-primary w-md waves-effect waves-light" type="submit">Log In</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-2 mb-0 row">
+                                        <div class="col-12 mt-4">
+                                            <a href="pages-recoverpw.html"><i class="mdi mdi-lock"></i> Forgot your password?</a>
+                                        </div>
+                                    </div>
+
+                                </form>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="mt-5 text-center">
+                        <p>Don't have an account ? <a href="pages-register.html" class="fw-medium text-primary"> Signup now </a> </p>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- JAVASCRIPT -->
+    <script src="assets/libs/jquery/jquery.min.js"></script>
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/node-waves/waves.min.js"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <script src="assets/js/app.js"></script>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+</body>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
---
+++ resources/views/backend/layouts/left_bar.blade.php
@@ -0,0 +1,95 @@
+<div class="leftside-menu">
+
+            <!-- Brand Logo Light -->
+            <a href="index.html" class="logo logo-light">
+                <span class="logo-lg">
+                    <img src="{{ url('') }}/dist/assets/images/logo.png" alt="logo">
+                </span>
+                <span class="logo-sm">
+                    <img src="{{ url('') }}/dist/assets/images/logo-sm.png" alt="small logo">
+                </span>
+            </a>
+
+            <!-- Brand Logo Dark -->
+            <a href="index.html" class="logo logo-dark">
+                <span class="logo-lg">
+                    <img src="{{ url('') }}/dist/assets/images/logo-dark.png" alt="dark logo">
+                </span>
+                <span class="logo-sm">
+                    <img src="{{ url('') }}/dist/assets/images/logo-sm.png" alt="small logo">
+                </span>
+            </a>
+
+            <!-- Sidebar -left -->
+            <div class="h-100" id="leftside-menu-container" data-simplebar>
+                <!--- Sidemenu -->
+                <ul class="side-nav">
+                    <li class="side-nav-item">
+                        <a data-bs-toggle="collapse" href="#sidebarForms" aria-expanded="false"
+                            aria-controls="sidebarForms" class="side-nav-link">
+                            <i class="ri-survey-line"></i>
+                            <span> Forms </span>
+                            <span class="menu-arrow"></span>
+                        </a>
+                        <div class="collapse" id="sidebarForms">
+                            <ul class="side-nav-second-level">
+                                <li>
+                                    <a href="form-elements.html">Basic Elements</a>
+                                </li>
+                                <li>
+                                    <a href="form-advanced.html">Form Advanced</a>
+                                </li>
+                                <li>
+                                    <a href="form-validation.html">Form Validation</a>
+                                </li>
+                                <li>
+                                    <a href="form-wizard.html">Form Wizard</a>
+                                </li>
+                                <li>
+                                    <a href="form-fileuploads.html">File Uploads</a>
+                                </li>
+                                <li>
+                                    <a href="form-editors.html">Form Editors</a>
+                                </li>
+                                <li>
+                                    <a href="form-image-crop.html">Image Crop</a>
+                                </li>
+                                <li>
+                                    <a href="form-x-editable.html">X Editable</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a data-bs-toggle="collapse" href="#sidebarTables" aria-expanded="false"
+                            aria-controls="sidebarTables" class="side-nav-link">
+                            <i class="ri-table-line"></i>
+                            <span> Tables </span>
+                            <span class="menu-arrow"></span>
+                        </a>
+                        <div class="collapse" id="sidebarTables">
+                            <ul class="side-nav-second-level">
+                                <li>
+                                    <a href="tables-basic.html">Basic Tables</a>
+                                </li>
+                                <li>
+                                    <a href="tables-datatable.html">Data Tables</a>
+                                </li>
+                                <li>
+                                    <a href="tables-editable.html">Editable Tables</a>
+                                </li>
+                                <li>
+                                    <a href="tables-responsive.html">Responsive Table</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+
+                </ul>
+                <!--- End Sidemenu -->
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
