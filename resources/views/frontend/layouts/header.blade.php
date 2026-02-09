38910781149ad1487cd5e783588013c4d3b0d83d
mrc-alif007-bd <mrc.alif007@gmail.com>
2026-02-09 12:06:47 +0600

    - Test Push

---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,72 @@
+<header class="header-area header-three">           	
+			  <div id="header-sticky" class="menu-area">
+                <div class="container-fluid pl-85 pr-85">
+                    <div class="second-menu">
+                        <div class="row align-items-center">
+                            <div class="col-xl-2 col-lg-2">
+                                <div class="logo">
+                                    <a href="{{ url('') }}/index.html"><img src="{{ url('') }}/assets/img/logo/logo.png" alt="logo"></a>
+                                </div>
+                            </div>
+                           <div class="col-xl-8 col-lg-8">
+                              
+                                <div class="main-menu text-center">
+                                    <nav id="mobile-menu">
+                                          <ul>
+                                            <li class="has-sub">
+                                                <a href="{{ url('') }}/index.html">Home</a>
+                                                <ul>													
+													<li><a href="{{ url('') }}/index.html">Home Page 01</a></li>
+													<li><a href="{{ url('') }}/index-2.html">Home Page 02</a></li>	
+												</ul>
+                                            </li>
+                                            <li><a href="{{ url('') }}/about.html">About</a></li>        
+                                            <li class="has-sub">
+                                                <a href="{{ url('') }}/room.html">our rooms</a>
+                                                <ul>													
+													<li> <a href="{{ url('') }}/room.html">Our Rooms</a></li>
+                                                    <li> <a href="{{ url('') }}/single-rooms.html">Rooms Details</a></li>
+												</ul>
+                                            </li>     
+                                            <li class="has-sub">
+                                                <a href="{{ url('') }}/services.html">Facilities</a>
+                                                <ul>													
+													<li> <a href="{{ url('') }}/services.html">Services</a></li>
+                                                    <li> <a href="{{ url('') }}/single-service.html">Services Details</a></li>
+												</ul>
+                                            </li>  
+                                              <li class="has-sub"><a href="{{ url('') }}/#">Pages</a>
+												<ul>
+                                                    <li><a href="{{ url('') }}/projects.html">Gallery</a></li>
+                                                    <li><a href="{{ url('') }}/faq.html">Faq</a></li>
+                                                    <li><a href="{{ url('') }}/team.html">Team</a></li>
+                                                    <li><a href="{{ url('') }}/team-single.html">Team Details</a></li>
+                                                    <li><a href="{{ url('') }}/pricing.html">Pricing</a></li>
+                                                    <li><a href="{{ url('') }}/shop.html">Shop</a></li>
+													<li><a href="{{ url('') }}/shop-details.html">Shop Details</a>
+                                                  </ul>
+											</li>
+                                            <li class="has-sub"> 
+                                                <a href="{{ url('') }}/blog.html">Blog</a>
+                                                <ul>
+                                                    <li><a href="{{ url('') }}/blog.html">Blog</a></li>
+                                                    <li><a href="{{ url('') }}/blog-details.html">Blog Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="{{ url('') }}/contact.html">Contact</a></li>                                               
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>   
+                             <div class="col-xl-2 col-lg-2 d-none d-lg-block">
+                                 <a href="{{ url('') }}/contact.html" class="top-btn mt-10 mb-10">reservation </a>
+                            </div>
+                            
+                                <div class="col-12">
+                                    <div class="mobile-menu"></div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
